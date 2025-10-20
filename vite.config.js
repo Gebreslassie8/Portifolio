049c1e2bde347426f591f7b9be3765d1c9bf7872
Gebreslassie Dessie <Gebreslassiedessie8@gmail.com>
2025-10-20 16:47:11 +0300

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: './', // Add this line for GitHub Pages
-//   build: {
-//     outDir: 'dist',
-//     sourcemap: false // Optional: disable for smaller build
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add this line for GitHub Pages
+  base: '/Portifolio/', // Add this line for GitHub Pages
   build: {
     outDir: 'dist',
     sourcemap: false // Optional: disable for smaller build
